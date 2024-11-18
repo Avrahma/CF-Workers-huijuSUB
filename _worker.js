57714666e60f8eb3a12c7ddb9ce2dfa827db490f
Avrahma <165910723+Avrahma@users.noreply.github.com>
2024-11-19 01:38:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,6 +20,7 @@ let urls = [];// https://subs.zeabur.app/clash , https://neko-warp.nloli.xyz/nek
 
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/Avrahma/edgetunnel/main/Clash/config/clash"; //订阅配置文件
+let subProtocol = 'https';
 
 export default {
 	async fetch (request,env) {
